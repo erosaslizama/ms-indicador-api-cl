8b2cd665f5543cfe192cdb69a7ab479403800961
erosas <erosas@DESKTOP-ND0581B>
2021-06-17 15:57:19 -0400

    init commit

--- src/main/java/com/workincode/indicador/controller/IndicadorController.java
+++ src/main/java/com/workincode/indicador/controller/IndicadorController.java
@@ -1,15 +1,12 @@
-package com.workincode.controller;
+package com.workincode.indicador.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndicadorController {
-	
 	@GetMapping("/saludo")
-	public String saludo() {
-		return "Saludo";
+	public String saludaRest() {
+		return "saludo";
 	}
-	
-
 }
