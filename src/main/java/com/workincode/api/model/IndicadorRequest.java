3c2beddab50d6eba9a5918f5156ea1646515ceb0
erosas <erosas@DESKTOP-ND0581B>
2021-06-17 18:48:50 -0400

    api indicardor

---
+++ src/main/java/com/workincode/api/model/IndicadorRequest.java
@@ -0,0 +1,98 @@
+package com.workincode.api.model;
+
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * IndicadorRequest
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2021-06-10T19:56:12.738-04:00[America/Santiago]")
+public class IndicadorRequest {
+	@JsonProperty("fecha")
+	private String fecha;
+
+	@JsonProperty("tipo")
+	private String tipo;
+
+	public IndicadorRequest fecha(String fecha) {
+		this.fecha = fecha;
+		return this;
+	}
+
+	/**
+	 * Get fecha
+	 * 
+	 * @return fecha
+	 */
+	@ApiModelProperty(example = "dd-mm-yyyy", value = "")
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public IndicadorRequest tipo(String tipo) {
+		this.tipo = tipo;
+		return this;
+	}
+
+	/**
+	 * Get tipo
+	 * 
+	 * @return tipo
+	 */
+	@ApiModelProperty(example = "uf", value = "")
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		IndicadorRequest indicadorRequest = (IndicadorRequest) o;
+		return Objects.equals(this.fecha, indicadorRequest.fecha)
+				&& Objects.equals(this.tipo, indicadorRequest.tipo);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(fecha, tipo);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("class IndicadorRequest {\n");
+
+		sb.append("    fecha: ").append(toIndentedString(fecha)).append("\n");
+		sb.append("    tipo: ").append(toIndentedString(tipo)).append("\n");
+		sb.append("}");
+		return sb.toString();
+	}
+
+	/**
+	 * Convert the given object to string with each line indented by 4 spaces
+	 * (except the first line).
+	 */
+	private String toIndentedString(Object o) {
+		if (o == null) {
+			return "null";
+		}
+		return o.toString().replace("\n", "\n    ");
+	}
+}
---
+++ src/main/java/com/workincode/indicador/api/IndicadorApiDelegate.java
@@ -0,0 +1,50 @@
+package com.workincode.indicador.api;
+
+
+import io.swagger.annotations.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.workincode.api.model.IndicadorRequest;
+import com.workincode.api.model.IndicadorResponse;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * A delegate to be called by the {@link IndicadorApiController}}.
+ * Implement this interface with a {@link org.springframework.stereotype.Service} annotated class.
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2021-06-10T19:56:12.738-04:00[America/Santiago]")
+public interface IndicadorApiDelegate {
+
+    default Optional<NativeWebRequest> getRequest() {
+        return Optional.empty();
+    }
+
+    /**
+     * POST /indicador/ : Obtiene el indicador deseado
+     *
+     * @param indicadorRequest Se envia una fecha y el indicador deseado por body (optional)
+     * @return obtiene el usuario (status code 200)
+     * @see IndicadorApi#indicadorPost
+     */
+    default ResponseEntity<IndicadorResponse> indicadorPost(IndicadorRequest indicadorRequest) {
+        getRequest().ifPresent(request -> {
+            for (MediaType mediaType: MediaType.parseMediaTypes(request.getHeader("Accept"))) {
+                if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
+                    String exampleString = "{ \"unidad\" : \"Pesos\", \"fecha\" : \"dd-mm-yyyy\", \"codigo\" : \"utm\", \"valor\" : 12.3, \"nombre\" : \"Unidad Tributaria Mensual (UTM)\" }";
+                    ApiUtil.setExampleResponse(request, "application/json", exampleString);
+                    break;
+                }
+            }
+        });
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+
+    }
+
+}
