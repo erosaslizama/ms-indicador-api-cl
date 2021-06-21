72a6eee0dcd08a20c4f501514631b2a077100513
erosas <erosas@DESKTOP-ND0581B>
2021-06-21 12:19:43 -0400

    comentarios para la clase repositorio

--- src/main/java/com/workincode/indicador/resource/repository/indicador/IndicadorH2Repository.java
+++ src/main/java/com/workincode/indicador/resource/repository/indicador/IndicadorH2Repository.java
@@ -8,15 +8,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import com.workincode.indicador.resource.repository.indicador.entity.IndicadorRepositoryEntity;
 
+
+
+
+
+/**
+ * Interface JPA
+ * 
+ * @author erosas
+ *
+ */
 @Repository
-public interface IndicadorH2Repository
-		extends
-			JpaRepository<IndicadorRepositoryEntity, UUID> {
+public interface IndicadorH2Repository extends JpaRepository<IndicadorRepositoryEntity, UUID> {
 	@Query("SELECT b FROM IndicadorRepositoryEntity b JOIN FETCH b.series as bs WHERE b.codigo = :codigo AND bs.fecha =:fecha")
-	 Optional<IndicadorRepositoryEntity> findByIndicador(@Param("codigo") String codigo,
-			@Param("fecha") Date fecha);
+	Optional<IndicadorRepositoryEntity> findByIndicador(@Param("codigo") String codigo, @Param("fecha") Date fecha);
 
 }
